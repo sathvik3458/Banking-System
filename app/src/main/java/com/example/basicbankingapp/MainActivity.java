7f6664966c43e3ebd23ed93df08f1e9b493850ca
Manish Wani <manishwani3@gmail.com>
2021-07-05 00:38:18 +0530

    Updated MainActivity

--- app/src/main/java/com/example/basicbankingapp/MainActivity.java
+++ app/src/main/java/com/example/basicbankingapp/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.basicbankingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+//  Variables
+    Button viewUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewUsers=findViewById(R.id.view_user_button);
+        viewUsers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ViewAllCustomers.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/basicbankingapp/ViewAllCustomers.java
@@ -0,0 +1,14 @@
+package com.example.basicbankingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewAllCustomers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_all_customers);
+    }
+}
